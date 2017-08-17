b1d729caaee87d313ae96d7c5d4d14d109ca0f62
Alexey Shatunov <shatunov.alexey@mail.ru>
2017-08-17 14:43:52 +0300

    3112-rev.1

--- pack_1_3112.js
+++ pack_1_3112.js
@@ -1,20 +1,17 @@
-let arr = [1, 2, 3, 4, 5];
-console.log("Initial array: ", arr);
+'use strict'
+
+const arr = [1, 2, 3, 4, 5];
+console.log('Initial array: ', arr);
 
 function drop(arr, i) {
 
-    //check whether array element index is out of range
-    if(i >= 0 && i < arr.length)
-        arr.splice(i, 1);
-    else
-        console.log("Choosen index ", i, "out of range!");
+    let replicatedArr = Array.from(arr);
+
+    replicatedArr.splice(i, 1);
+    return replicatedArr;
 }
 
-for(let elem_idx = 0; elem_idx < arr.length; elem_idx++) {
-    console.log("Index of element to be removed: ", elem_idx);
-    drop(arr, elem_idx);
-    console.log("Modified array: ", arr);
-}
-
-//testing drop function with index value that is out of arr range
-drop(arr, elem_idx);
+arr.forEach((currVal, currIdx, arr) => {
+    console.log('Element with index', currIdx, 'will be removed from array');
+    console.log(drop(arr, currIdx));
+});
