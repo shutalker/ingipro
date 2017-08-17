3ac53b5080b290827e3b3a4508e1417b1c3c261e
Alexey Shatunov <shatunov.alexey@mail.ru>
2017-08-17 15:59:09 +0300

    3514-rev.1

--- pack_1_3514.js
+++ pack_1_3514.js
@@ -1,24 +1,26 @@
-const arr1 = [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]; // 5
-const arr2 = [1,1,2,-2,5,2,4,4,-1,-2,5]; // -1
+'use strict'
+
+const arr1 = [20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]; // 5
+const arr2 = [1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]; // -1
 
 function searchOddRepeats(arr) {
-    //array that contains odd repeating numbers (my english is not very well)
-    let repeats = [];
+    //creating an empty object without any properties
+    const counterObj = Object.create(null);
+    const repeats = [];
 
-    arr.forEach(function countRepeats(currVal, currIdx, arr) {
-        //check whether the current number was detected as odd repeating
-        if(repeats.indexOf(currVal) === -1) {
-            let repeatCounter = 1;
-
-            for (i = 0; i < arr.length; i++) {
-                if ((i !== currIdx) && (arr[i] === currVal))
-                    repeatCounter++;
-            }
-
-            if (repeatCounter % 2 !== 0)
-                repeats.push(currVal);
+    arr.forEach((currVal, currIdx, arr) => {
+        if (Object.prototype.hasOwnProperty.call(counterObj, currVal)) {
+            counterObj[currVal]++;
+        } else {
+            counterObj[currVal] = 1;
         }
     });
+
+    for (let prop in counterObj) {
+        if (counterObj[prop] % 2) {
+            repeats.push(+prop);
+        }
+    }
 
     return repeats;
 }
